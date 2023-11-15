f22b7bd2540e27bf00d3d32d61769ce56a597272
Matthew Setter <matthew@matthewsetter.com>
2023-11-15 16:01:53 +1000

    Strip the handler and factory back to bare essentials
    This is a small change to remove most of the boilerplate that came with
    the generated handler and handler factor, in preparation for them to
    work with the new core logic.

--- test/AppTest/Handler/HomePageHandlerTest.php
+++ test/AppTest/Handler/HomePageHandlerTest.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 namespace AppTest\Handler;
 
 use App\Handler\HomePageHandler;
-use Laminas\Diactoros\Response\HtmlResponse;
 use Laminas\Diactoros\Response\JsonResponse;
-use Mezzio\Router\RouterInterface;
-use Mezzio\Template\TemplateRendererInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
@@ -16,51 +13,28 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class HomePageHandlerTest extends TestCase
 {
-    /** @var ContainerInterface&MockObject */
-    protected $container;
-
-    /** @var RouterInterface&MockObject */
-    protected $router;
+    protected MockObject|ContainerInterface $container;
 
     protected function setUp(): void
     {
-        $this->container = $this->createMock(ContainerInterface::class);
-        $this->router    = $this->createMock(RouterInterface::class);
+        $this->container = $this->createMock(
+            ContainerInterface::class
+        );
     }
 
-    public function testReturnsJsonResponseWhenNoTemplateRendererProvided(): void
+    public function testReturnsJsonResponse(): void
     {
-        $homePage = new HomePageHandler(
-            $this->container::class,
-            $this->router,
-            null
-        );
+        $homePage = new HomePageHandler();
         $response = $homePage->handle(
-            $this->createMock(ServerRequestInterface::class)
+            $this->createMock(
+                ServerRequestInterface::class
+            )
         );
 
-        self::assertInstanceOf(JsonResponse::class, $response);
-    }
-
-    public function testReturnsHtmlResponseWhenTemplateRendererProvided(): void
-    {
-        $renderer = $this->createMock(TemplateRendererInterface::class);
-        $renderer
-            ->expects($this->once())
-            ->method('render')
-            ->with('app::home-page', $this->isType('array'))
-            ->willReturn('');
-
-        $homePage = new HomePageHandler(
-            $this->container::class,
-            $this->router,
-            $renderer
+        self::assertInstanceOf(
+            JsonResponse::class,
+            $response
         );
-
-        $response = $homePage->handle(
-            $this->createMock(ServerRequestInterface::class)
-        );
-
-        self::assertInstanceOf(HtmlResponse::class, $response);
+        self::assertSame('""', $response->getBody()->getContents());
     }
 }
