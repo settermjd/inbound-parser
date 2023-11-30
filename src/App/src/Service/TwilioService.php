306df866a5da25593a5041e4c386b80db0b42dd5
Matthew Setter <matthew@matthewsetter.com>
2023-11-30 14:45:17 +1000

    Finish the first version of TwilioService
    This change refactors a lot of functionality from HomePageHandler for
    sending a new note notification into this class, as it seemed to be a
    more logical place to put it.

--- src/App/src/Service/TwilioService.php
+++ src/App/src/Service/TwilioService.php
@@ -4,30 +4,70 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\User;
+use Laminas\Mime\Part;
 use Twilio\Exceptions\TwilioException;
 use Twilio\Rest\Api\V2010\Account\MessageInstance;
 use Twilio\Rest\Client;
 
 class TwilioService implements SmsServiceInterface
 {
+    private string $templateMessageWithAttachment = <<<EOF
+Hi %s. This a quick confirmation that "%s" has been added as a note on your account, along with the text, which you can find in the attachment to this SMS.
+EOF;
+
+    private string $templateMessageWithAttachments = <<<EOF
+Hi %s. This a quick confirmation that "%s" have been added as a note on your account, along with the text, which you can find in the attachment to this SMS.
+EOF;
+
     public function __construct(
-        private readonly Client $client
+        private readonly Client $client,
+        private readonly string $sender
     ) {
     }
 
     /**
-     * @return MessageInstance
+     * @param Part[] $attachments
      * @throws TwilioException
      */
-    public function sendSMS(string $message, string $recipient, string $sender)
+    public function sendNewNoteNotification(
+        User $recipient,
+        array $attachments = [],
+    ): bool
     {
-        return $this->client
-            ->messages
-            ->create($recipient,
+        $bodyTemplate = $this->templateMessageWithAttachments;
+        $message = $this->client->messages
+            ->create($recipient->getPhoneNumber(),
                 [
-                    "body" => $message,
-                    "sender" => $sender
+                    "body" => sprintf(
+                        $bodyTemplate,
+                        $recipient->getName(),
+                        implode(
+                            ', ',
+                            array_map(
+                                fn(Part $value): string => $value->getFilename() ?? '',
+                                $attachments
+                            )
+                        )
+                    ),
+                    "sender" => $this->sender
                 ]
             );
+
+        return in_array(
+            $message->status,
+            [
+                'accepted',
+                'delivered',
+                'queued',
+                'read',
+                'received',
+                'receiving',
+                'scheduled',
+                'sending',
+                'sent',
+                'undelivered',
+            ]
+        );
     }
 }
--- test/AppTest/Service/TwilioServiceTest.php
+++ test/AppTest/Service/TwilioServiceTest.php
@@ -2,7 +2,9 @@
 
 namespace AppTest\Service;
 
+use App\Entity\User;
 use App\Service\TwilioService;
+use Laminas\Mime\Part;
 use PHPUnit\Framework\TestCase;
 use Twilio\Rest\Api\V2010\Account\MessageInstance;
 use Twilio\Rest\Api\V2010\Account\MessageList;
@@ -12,9 +14,16 @@ class TwilioServiceTest extends TestCase
 {
     public function testCanSendConfirmationSMS()
     {
-        $message = "Here is the message";
-        $recipient = "+6140912341234";
+        $message = "Hi %s. This a quick confirmation that \"%s\" have been added as a note on your account, along with the text, which you can find in the attachment to this SMS.";
+        $recipient = new User(
+            "Dave Grohl",
+            "dave@foofighters.example.com",
+            "+6140912341234"
+        );
         $sender = "+6140912341244";
+
+        $messageInstance = $this->createMock(MessageInstance::class);
+        $messageInstance->status = 'queued';
 
         $client = $this->createMock(Client::class);
         $client->messages = $this->createMock(MessageList::class);
@@ -22,24 +31,34 @@ class TwilioServiceTest extends TestCase
             ->expects($this->once())
             ->method('create')
             ->with(
-                $recipient,
+                $recipient->getPhoneNumber(),
                 [
-                    'body' => $message,
+                    'body' => sprintf(
+                        $message,
+                        $recipient->getName(),
+                        "test document.pdf"
+                    ),
                     'sender' => $sender,
                 ]
             )
             ->willReturn(
-                $this->createMock(MessageInstance::class)
+                $messageInstance
             );
 
-        $twilioService = new TwilioService($client);
+        $twilioService = new TwilioService($client, $sender);
 
-        $this->assertInstanceOf(
-            MessageInstance::class,
-            $twilioService->sendSMS(
-                message: $message,
+        $attachment = $this->createMock(Part::class);
+        $attachment
+            ->expects($this->once())
+            ->method('getFileName')
+            ->willReturn('test document.pdf');
+
+        $this->assertTrue(
+            $twilioService->sendNewNoteNotification(
                 recipient: $recipient,
-                sender: $sender,
+                attachments: [
+                    $attachment
+                ]
             )
         );
     }
